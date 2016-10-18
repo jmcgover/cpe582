f0bca87edd72cd2981b999b1bf2216ab0c4c45ca
jmcgover <jmcgover@calpoly.edu>
2016-10-18 14:03:01 -0700

    got it classifying and reverted the data folder

--- projects/1project/restaurant.py
+++ projects/1project/restaurant.py
@@ -25,12 +25,16 @@ LOGGER.setLevel(logging.WARNING)
 
 # Natual Language Processing
 import nltk
+from nltk.tokenize import word_tokenize
+from nltk import ngrams
 from bs4 import BeautifulSoup
 from pprint import pformat
 import re
 
 # Maths
 import random
+from collections import defaultdict
+import string
 
 DESCRIPTION = """Machine learning algorithms to predict paragraph rating and topic, overall rating, and authorship of restaurant reviews written by students in CPE 582 Fall 2016. Written by Jeff McGovern (jmcgover@calpoly.edu)."""
 def get_arg_parser():
@@ -41,13 +45,15 @@ def get_arg_parser():
     parser.add_argument('-d', '--debug',
             action='store_true', default=False,
             help='enters debug logging mode, adding details to the log message')
+    parser.add_argument('-i', '--info',
+            action='store_true', default=False,
+            help='enters info logging mode')
     parser.add_argument('-t', '--test',
             nargs = '?',
             help='test the review parsing of a single file')
     return parser
 
 def enter_debug_mode():
-    print("FUCK")
     verbose_handler = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s:%(funcName)s:%(lineno)s:%(levelname)s:%(message)s')
     verbose_handler.setFormatter(formatter)
@@ -55,10 +61,55 @@ def enter_debug_mode():
     LOGGER.addHandler(verbose_handler)
     LOGGER.setLevel(logging.DEBUG)
 
+PUNCTUATION = set(string.punctuation)
+def extract_author_ngrams(reviews, low, high):
+    ngram_counts = defaultdict(int)
+
+    for r in reviews:
+        text = ' '.join(r.paragraphs)
+        tokens = word_tokenize(text)
+        words = [t for t in tokens if t not in PUNCTUATION]
+        for n in range(low, high + 1):
+            all_ngrams = ngrams(words, n)
+            for gram in all_ngrams:
+                ngram_counts[gram] += 1
+    return ngram_counts
+
+def exercise4(dataset):
+    n_low = 1
+    n_high = 2
+
+    LOGGER.info('Building datasets...')
+    # Build Training Set
+    train_author_reviews = defaultdict(list)
+    for review in dataset.get_training():
+        train_author_reviews[review.author].append(review)
+    # Build Testing Set
+    test_author_reviews = defaultdict(list)
+    for review in dataset.get_testing():
+        test_author_reviews[review.author].append(review)
+
+    LOGGER.info('Building features...')
+    # Build Training Features
+    train_author_ngrams = []
+    for author in train_author_reviews:
+        counts = extract_author_ngrams(train_author_reviews[author], n_low, n_high)
+        train_author_ngrams.append((counts, author))
+    # Build Testing Features
+    test_author_ngrams = []
+    for author in test_author_reviews:
+        counts = extract_author_ngrams(test_author_reviews[author], n_low, n_high)
+        test_author_ngrams.append((counts, author))
+    LOGGER.info('Building classifier...')
+    classifier = nltk.NaiveBayesClassifier.train(train_author_ngrams)
+    print("Accuracy: ", nltk.classify.accuracy(classifier, test_author_ngrams))
+    return
 
 def main():
     arg_parser = get_arg_parser()
     args = arg_parser.parse_args()
+    if args.info == True:
+        LOGGER.setLevel(logging.INFO)
     if args.debug == True:
         enter_debug_mode()
     data_dir = args.data_dir
@@ -69,9 +120,10 @@ def main():
         LOGGER.info('Found:%s', pformat(relevant))
         reviews = [Review(name) for name in relevant]
         for r in reviews:
-            [LOGGER.info('%s:%d:%s',r.path,i,p) for i,p in enumerate(r.paras)]
+            [LOGGER.info('%s:%d:%s',r.path,i,p) for i,p in enumerate(r.all_paras)]
     else:
         dataset = ReviewDataset(data_dir)
+        exercise4(dataset)
     return 0
 
 if __name__ == '__main__':
--- projects/1project/review.py
+++ projects/1project/review.py
@@ -24,35 +24,108 @@ import re
 
 # Maths
 import random
+from collections import defaultdict
+
+def str_to_int(x):
+    assert isinstance(x, str)
+    return int(round(float(x)))
+def parse_rating(x):
+    assert isinstance(x, str)
+    return str_to_int(re.findall('\d+\.{0,1}\d*',x)[0])
 
 class Review(object):
     FOOD_STR    = 'FOOD'
     VENUE_STR   = 'VENUE'
     SERVICE_STR = 'SERVICE'
     OVERALL_STR = 'OVERALL'
+    RATING_STR  = 'RATING'
     FOOD_NDX    = 0
     VENUE_NDX   = 1
     SERVICE_NDX = 2
     OVERALL_NDX = 3
+    ALL_RATING_STR = (FOOD_STR, VENUE_STR, SERVICE_STR, OVERALL_STR)
 
-    def __init__(self, filename):
-        self.path = filename
+    def __init__(self, path):
+        self.path = path
+
+        # Open and BeautifulSoup
+        soup = None
+        LOGGER.debug("Opening %s...", path)
+        with open(path, 'r') as file:
+            LOGGER.debug("BeautifulSouping...")
+            soup = BeautifulSoup(file, 'lxml')
+        self.raw = soup.prettify()
+
+        # Get parent directory to handle specific cases
+        self.parent_dir = os.path.dirname(path)
+
+        # Extract Paragraphs
+        self.all_paras = Review.get_paragraphs(soup, self.parent_dir)
+        self.fill_properties()
+        return
+    def __str__(self):
+        return self.raw
+    def fill_properties(self):
+        [LOGGER.debug('PARAGRAPH:%d:%s', i, p) for i,p in enumerate(self.all_paras)]
+        if len(self.all_paras) != 13:
+            LOGGER.error('Parsed %d paragraphs for %s',len(self.all_paras),self.path)
+            [LOGGER.error('PARAGRAPH:%d:%s', i, p) for i,p in enumerate(self.all_paras)]
+        else:
+            LOGGER.debug('Parsed %d paragraphs for %s',len(self.all_paras),self.path)
+        assert len(self.all_paras) == 13, 'Received {} paragraphs, need 13'.format(len(self.all_paras))
         self.author = None
         self.restaurant = None
         self.address = None
         self.city = None
-        self.paragraphs = []
-        self.ratings = []
-        self.raw = None
-
-        # Open and BeautifulSoup
-        soup = None
-        LOGGER.debug("Opening %s...", filename)
-        with open(filename, 'r') as file:
-            LOGGER.debug("BeautifulSouping...")
-            soup = BeautifulSoup(file, 'lxml')
-
+        self.paragraphs = self.all_paras[-4:]
+        self.ratings = [None] * 4
+        LOGGER.debug(self.parent_dir)
+        for i,p in enumerate(self.all_paras[:-4]):
+            if 'reviewer' in p.lower():
+                author = p.replace('REVIEWER','').replace(':','').strip()
+                LOGGER.debug('Author: %s', author)
+                self.author = author
+            if 'name' in p.lower():
+                restaurant = p.lower().replace('name','').replace(':','').strip()
+                LOGGER.debug('Restaurant: %s', restaurant)
+                self.restaurant = restaurant
+            if 'address' in p.lower():
+                address = p.lower().replace('address','').replace(':','').strip()
+                LOGGER.debug('Address: %s', address)
+                self.address = address
+            if 'city' in p.lower():
+                city = p.lower().replace('city','').replace(':','').strip()
+                LOGGER.debug('City: %s', self.city)
+                self.city = city
+            if Review.FOOD_STR in p:
+                rating = parse_rating(p)
+                LOGGER.debug('Food Rating:%d', rating)
+                self.ratings[Review.FOOD_NDX] = rating
+            if Review.VENUE_STR in p:
+                rating = parse_rating(p)
+                LOGGER.debug('Venue Rating:%d', rating)
+                self.ratings[Review.VENUE_NDX] = rating
+            if Review.SERVICE_STR in p:
+                rating = parse_rating(p)
+                LOGGER.debug('Service Rating:%d', rating)
+                self.ratings[Review.SERVICE_NDX] = rating
+            if any(x in p for x in (Review.OVERALL_STR, Review.RATING_STR)):
+                rating = parse_rating(p)
+                LOGGER.debug('Overall Rating:%d', rating)
+                self.ratings[Review.OVERALL_NDX] = rating
+        assert self.author, 'No author for %s' % self.path
+        assert self.city, 'No city for %s:%s' % (self.path, self.city)
+        assert self.address, 'No address for %s:%s' % (self.path, self.address)
+        assert self.restaurant, 'No restaurant for %s:%s' % (self.path, self.restaurant)
+        for ndx, r in enumerate(self.ratings):
+            assert r != None, 'Failed to parse %s rating' % Review.ALL_RATING_STR[ndx]
+        return
+    @staticmethod
+    def get_paragraphs(soup, parent_dir):
         # EXTRACT PARAGRAPHS
+        paras = []
+
+        # Replace br's
         LOGGER.debug('Replacing br\'s...')
         for br in soup.find_all('br'):
             br.replace_with('\n')
@@ -60,21 +133,35 @@ class Review(object):
         LOGGER.debug('Getting body...')
         body = soup.find('body')
 
+        # Split by either <p> or <span>
+        parent_dir = parent_dir
         raw_paragraphs = []
         LOGGER.debug('Getting paragraphs...')
-        if 'Gavin Scott' in self.path and 'Review1' in self.path:
-            LOGGER.debug('Using <span> on Gavin\'s Review1...')
-            soup_paras = body.find_all('span')
-        elif 'Daniel Kauffman' in self.path and 'Review2' in self.path:
-            LOGGER.debug('Using <span> on Daniel\'s Review2...')
-            soup_paras = body.find_all('span')
-        elif 'Daniel Kauffman' in self.path and 'Review3' in self.path:
-            LOGGER.debug('Using <span> on Daniel\'s Review3...')
+        if any((
+                    all(x in parent_dir for x in ('Gavin Scott', 'Review1')),
+                    all(x in parent_dir for x in ('Daniel Kauffman', 'Review2')),
+                    all(x in parent_dir for x in ('Daniel Kauffman', 'Review3'))
+                    )):
+            LOGGER.debug('Using <span> on {}...'.format(parent_dir))
             soup_paras = body.find_all('span')
         else:
             LOGGER.debug('Using <p> to parse paragraphs...')
             soup_paras = body.find_all('p')
-        if 'Jonathan Sleep' in self.path and 'Review3' in self.path:
+
+        if all(x in parent_dir for x in ('Alanna Buss', 'Review2')):
+            LOGGER.debug('')
+            body_string = str(body)
+            LOGGER.debug('BEFORE:{}'.format(body_string))
+            for i,p in enumerate(soup_paras):
+                LOGGER.debug('PTAG:{}:{}'.format(i,str(p)))
+                body_string = body_string.replace(str(p), '')
+            body_string = body_string.replace('<body>','<p>')
+            body_string = body_string.replace('</body>','</p>')
+            LOGGER.debug('AFTER:{}'.format(body_string))
+            soup_paras[10] = BeautifulSoup(body_string, 'lxml')
+
+        # Gather text
+        if all(x in parent_dir for x in ('Jonathan Sleep', 'Review3')):
             for p in soup_paras:
                 LOGGER.debug('Found a paragraph...')
                 raw_paragraphs.append(p.get_text())
@@ -86,6 +173,8 @@ class Review(object):
             else:
                 LOGGER.debug('Could not find paragraphs...')
                 raw_paragraphs.extend(body.get_text().split('\n'))
+
+        # Build list of paragraphs
         LOGGER.debug('RAW_PARAGRAPHS:%s', raw_paragraphs)
         paragraphs = []
         for p in raw_paragraphs:
@@ -95,53 +184,51 @@ class Review(object):
             else:
                 LOGGER.debug('APPENDING RAW:%s', p)
                 paragraphs.append(p)
-        if 'Jon Doughty' in self.path:
-            LOGGER.debug('Collapsing Jon\'s paragraphs...')
-            self.paras = Review.collapse_paragraphs(paragraphs)
-        elif 'Christian Durst' in self.path and 'Review1' in self.path:
-            LOGGER.debug('Collapsing Skylar\'s Review1 paragraphs...')
-            self.paras = Review.collapse_paragraphs(paragraphs)
-        elif 'Joseph Wilson' in self.path and 'Review1' in self.path:
-            LOGGER.debug('Collapsing Joey\'s Review1 paragraphs...')
-            self.paras = Review.collapse_paragraphs(paragraphs)
-        elif 'Ryan Smith' in self.path and 'Review2' in self.path:
-            LOGGER.debug('Collapsing Ryan\'s Review2 paragraphs...')
-            self.paras = Review.collapse_paragraphs(paragraphs, '')
-        elif 'Christian Durst' in self.path and 'Review3' in self.path:
-            LOGGER.debug('Collapsing Skylar\'s Review3 paragraphs...')
-            self.paras = Review.collapse_paragraphs(paragraphs, all=True)
-        else:
-            self.paras = [p for p in paragraphs if p]
+        if any((
+                    all(x in parent_dir for x in ('Jon Doughty',)),
+                    all(x in parent_dir for x in ('Joseph Wilson', 'Review1'))
+                    )):
+            LOGGER.debug('Collapsing {}...'.format(parent_dir))
+            paras = Review.collapse_paragraphs(paragraphs)
+        elif 'Ryan Smith' in parent_dir and 'Review2' in parent_dir:
+            LOGGER.debug('Collapsing Ryan\'s Review2 paragraphs, joining without space...')
+            paras = Review.collapse_paragraphs(paragraphs, '')
+        elif 'Christian Durst' in parent_dir and ('Review3' in parent_dir or 'Review1' in parent_dir):
+            LOGGER.debug('Collapsing ALL of Skylar\'s Review3 paragraphs...')
+            paras = Review.collapse_paragraphs(paragraphs, all=True)
+            if 'Review1' in parent_dir:
+                paras = paras[0:8] + ['WRITTEN REVIEW:'] + [paras[8].replace('WRITTEN REVIEW:','').strip()] + paras[9:]
 
-        if 'Justin Postigo' in self.path and 'Review3' in self.path:
+        else:
+            paras = [p for p in paragraphs if p]
+
+        if all( x in parent_dir for x in ('Justin Postigo', 'Review3')):
             LOGGER.debug('Appending empty paragraph to Justin\'s Review3...')
-            self.paras.append('')
-        if 'Samuel Lakes' in self.path and 'Review2' in self.path:
+            paras.append('')
+        if all( x in parent_dir for x in ('Jeffrey McGovern',)):
+            paras = paras[0:3] \
+                + [paras[3].replace(', CA','').replace(', WA','')]\
+                + paras[4:]
+        if all( x in parent_dir for x in ('Samuel Lakes', 'Review1')):
+            paras = paras[0:2] \
+                + [paras[2].replace(', San Luis Obispo, CA 93401','')]\
+                + paras[3:]
+        if all( x in parent_dir for x in ('Samuel Lakes', 'Review2')):
             LOGGER.debug('Fixing CITY in Sam\'s Review2...')
-            self.paras = self.paras[0:2] \
-                + [self.paras[2].replace(', San Luis Obispo, CA 93405','')]\
+            paras = paras[0:2] \
+                + [paras[2].replace(', San Luis Obispo, CA 93405','')]\
                 + ['CITY: San Luis Obispo'] \
-                + self.paras[3:]
-        if 'Ivan Pachev' in self.path and 'Review2' in self.path:
+                + paras[3:]
+        if all( x in parent_dir for x in ('Ivan Pachev', 'Review2')):
             LOGGER.debug('Combining some paragraphs of Ivan\'s Review2...')
-            self.paras[11] = ' '.join(self.paras[11:13])
-            self.paras[12] = ' '.join(self.paras[14:])
-            del self.paras[13:]
-
-
-        LOGGER.debug('Prettifying...')
-        self.raw = soup.prettify()
-        LOGGER.debug('PRETTY:%s', self.raw)
-        [LOGGER.debug('PARAGRAPH:%d:%s', i, p) for i,p in enumerate(self.paras)]
-        if len(self.paras) != 13:
-            LOGGER.error('Parsed %d paragraphs for %s',len(self.paras),self.path)
-            [LOGGER.error('PARAGRAPH:%d:%s', i, p) for i,p in enumerate(self.paras)]
-        else:
-            LOGGER.debug('Parsed %d paragraphs for %s',len(self.paras),self.path)
-        assert(len(self.paras) == 13)
-        return
-    def __str__(self):
-        return self.raw
+            paras[11] = ' '.join(paras[11:13])
+            paras[12] = ' '.join(paras[14:])
+            del paras[13:]
+        if 'Christian Durst' in parent_dir:
+            paras[0] = 'REVIEWER: Christian Durst'
+        if 'Foaad Khosmood' in paras[0]:
+            paras[0] = 'REVIEWER: Aditya Budhwar'
+        return paras
     @staticmethod
     def collapse_paragraphs(paragraphs, text_delimiter = ' ', all=False):
         collapsed_paras = []
@@ -186,6 +273,10 @@ class Review(object):
             del collapsed_paras[13:]
         return collapsed_paras
 
+class ReviewClassifier(object):
+    def __init__(self, review_dataset):
+        return
+
 class ReviewDataset(object):
     TEST_DIR = 'test'
     TRAIN_DIR = 'training'
@@ -194,25 +285,113 @@ class ReviewDataset(object):
     DIR_REGEX = re.compile('{}|{}|{}'.format(TEST_DIR, TRAIN_DIR, REVIEW_DIR))
 
     def __init__(self, dir_path, training_portion = 0.30):
-        self.test = set()
-        self.train = set()
+        self.test = []
+        self.train = []
+        self.good_paragraphs= defaultdict(list)
+        self.bad_paragraphs = defaultdict(list)
+        self.rating_reviews = defaultdict(list)
+        self.author_reviews = defaultdict(list)
+
         # Validate and extract dirs
         folders = ReviewDataset.extract_dirs(dir_path)
         LOGGER.debug('DISCOVERED FOLDERS:{}'.format(folders))
+
+        # Extract all filenames
         filenames = {}
         for folder in folders:
             filenames[folder] = ReviewDataset.get_lowest_filenames(folder)
         LOGGER.debug('DISCOVERED FILES:{}'.format(pformat(filenames)))
+
+        # Build Reviews
         all_reviews = []
         reviews = {}
         for folder in filenames:
             new_reviews = ReviewDataset.build_reviews(filenames[folder])
             all_reviews.extend(new_reviews)
             reviews[folder] = new_reviews
-        for review in sorted(all_reviews, reverse=True, key=lambda x: len(x.paras)):
-            LOGGER.debug('%s PARAGRAPHS: %d', review.path, len(review.paras))
-            [LOGGER.debug('%d:%s', i, para) for i,para in enumerate(review.paras)]
+
+        # Build Test and Training Sets
+        if all(x in reviews for x in (ReviewDataset.TEST_DIR, ReviewDataset.TRAIN_DIR)):
+            self.test.append(reviews[ReviewDataset.TEST_DIR])
+            self.train.append(reviews[ReviewDataset.TRAIN_DIR])
+        else:
+            shuffled_reviews = [a for a in all_reviews]
+            random.shuffle(shuffled_reviews)
+            self.test  = shuffled_reviews[:int(training_portion*len(shuffled_reviews))]
+            self.train = shuffled_reviews[int(training_portion*len(shuffled_reviews)):]
+
+        # Sanitize Reviews
+        ReviewDataset.sanitize_reviews(all_reviews)
+
+        # Build collections
+        for review in self.train:
+            self.author_reviews[review.author].append(review)
+            self.rating_reviews[str(review.ratings[Review.OVERALL_NDX])].append(review)
+            for ndx, para in enumerate(review.paragraphs):
+                if review.ratings[ndx] >=4:
+                    self.good_paragraphs[str(review.ratings[ndx])].append(para)
+                if review.ratings[ndx] >=4:
+                    self.bad_paragraphs[str(review.ratings[ndx])].append(para)
+
         LOGGER.debug('Count: %d', len(all_reviews))
+    def get_training(self):
+        return self.train
+    def get_testing(self):
+        return self.test
+
+    @staticmethod
+    def sanitize_reviews(reviews):
+        for review in reviews:
+            review.city = review.city.replace(', CA','').replace(', WA','').title()
+            review.restaurant = review.restaurant.replace('é','e')
+            if review.restaurant == 'chili peppers':
+                review.restaurant = 'chillie peppers restaurant'
+            if review.restaurant == 'central coasters':
+                review.restaurant = 'central coaster'
+            if review.restaurant == 'oki momo':
+                review.restaurant = 'okimomo asian grill'
+            if review.restaurant == 'fatoush':
+                review.restaurant = 'fattoush'
+            if review.restaurant == 'slo brew':
+                review.restaurant = 'slo brewing company'
+        return
+
+    @staticmethod
+    def build_reviews(filenames):
+        reviews = []
+        for filename in filenames:
+            LOGGER.debug('PARSING:%s', filename)
+            try:
+                new_review = Review(filename)
+                reviews.append(new_review)
+            except Exception as e:
+                LOGGER.warning('Failed to parse "%s": %s', filename, e)
+        return reviews
+
+    @staticmethod
+    def get_lowest_filenames(dir):
+        lowest_files = []
+        for root, dirs, filenames in os.walk(str(dir)):
+            for filename in filenames:
+                full_path = os.path.join(root, filename)
+                lowest_files.append(full_path)
+        return lowest_files
+
+    @staticmethod
+    def check_dir(path, exit_program):
+        if not os.path.exists(path):
+            LOGGER.error("Directory '%s' does not exist", path)
+            if exit_program:
+                sys.exit(errno.ENOENT)
+            else:
+                return False
+        if not os.path.isdir(path):
+            LOGGER.error("Directory '%s' is not a directory", path)
+            if exit_program:
+                sys.exit(errno.ENOTDIR)
+            else:
+                return False
+        return True
 
     @staticmethod
     def extract_dirs(path):
@@ -257,42 +436,3 @@ class ReviewDataset(object):
             LOGGER.error('Could not find any directory of the style \'{}\''.format(REVIEW_DIR))
             sys.exit(errno.ENOTDIR)
         return None
-
-    @staticmethod
-    def check_dir(path, exit_program):
-        if not os.path.exists(path):
-            LOGGER.error("Directory '%s' does not exist", path)
-            if exit_program:
-                sys.exit(errno.ENOENT)
-            else:
-                return False
-        if not os.path.isdir(path):
-            LOGGER.error("Directory '%s' is not a directory", path)
-            if exit_program:
-                sys.exit(errno.ENOTDIR)
-            else:
-                return False
-        return True
-
-    @staticmethod
-    def get_lowest_filenames(dir):
-        lowest_files = []
-        for root, dirs, filenames in os.walk(str(dir)):
-            for filename in filenames:
-                full_path = os.path.join(root, filename)
-                lowest_files.append(full_path)
-        return lowest_files
-
-    @staticmethod
-    def build_reviews(filenames):
-        reviews = []
-        for filename in filenames:
-            LOGGER.debug('PARSING:%s', filename)
-            try:
-                new_review = Review(filename)
-                reviews.append(new_review)
-            except Exception as e:
-                LOGGER.warning('Failed to parse "%s": %s', filename, e)
-        return reviews
-
-
